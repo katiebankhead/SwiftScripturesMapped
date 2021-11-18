046799808aff3396bb5a691eb3bde7d8ab97d453
Katie Bankhead <katie@bankheads.net>
2021-11-18 08:40:04 -0700

    Initial Commit

---
+++ ScripturesMapped/ScripturesMappedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScripturesMappedApp.swift
+//  ScripturesMapped
+//
+//  Created by Katie Bankhead on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct ScripturesMappedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
